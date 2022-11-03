7b74997517fd7795409c46776d070c79df1c8c51
Maxoverking <avemax7@gmail.com>
2022-11-03 23:56:07 +0100

    ждет коректировки

--- src/index.js
+++ src/index.js
@@ -3,85 +3,96 @@ import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
-const input = document.getElementById('search-box');
+//============================================================
 
 const countryList = document.querySelector('.country-list');
 countryList.style.display = 'flex';  
 countryList.style.flexDirection = 'column';
 
+//============================================================
+
 const countryInfo = document.querySelector('.country-info');
+
+//============================================================
+
+const input = document.getElementById('search-box');
 
 input.addEventListener('input', debounce( getInputValue, DEBOUNCE_DELAY));
 
 function getInputValue(evt) {
     evt.preventDefault();
-    const inputValue = evt.target.value;
 
-    if (inputValue.length === 0 ) {
-        countryList.innerHTML = '';
-        countryInfo.innerHTML = '';
-    } else if (inputValue.length === 1) {
-        Notify.info("Too many matches found. Please enter a more specific name.")
-        return;
-    } else {
-        const url = "https://restcountries.com/v2/name";
-        const field = "fields=name,capital,population,flags,languages";
-
+    const inputValue = evt.target.value.trim();
+    
+    const url = "https://restcountries.com/v2/name";
+    const field = "fields=name,capital,population,flags,languages";
+    
     fetch(`${url}/${inputValue}?${field}`)
             .then(response => 
             response.json()
             ).then(data => {       
-            goToOpenApiArray(data);       
+                goToOpenApiArray(data); 
+
+                console.log("🚀 ~ data", data);              
             })
             .catch(error => {
             console.log(error);
-            })      
-        }
+            })   
+     
 }
 
 function goToOpenApiArray(data) {
-    data.forEach(element => {
+    console.log("🚀 ~ data", data.length);  
 
-        const { flags, name} = element;
+    if (data.length >= 10) {
+        Notify.info("Too many matches found. Please enter a more specific name.");
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+         return;
+    }else if (data.length === undefined) {
+        countryList.innerHTML = '';
+        return; 
+    }
+    
+    data.forEach(element => {
+        const {flags, name} = element;
 
         let languagesArray = [];
-        element.languages.forEach((el, ind, array) => {
+
+        element.languages.forEach(el => {
             const allValueLanguages = [Object.values(el.name).join('')];
             languagesArray.push(...allValueLanguages);    
         });
-        const allLanguagesAvailable = languagesArray.join(', ');    
-
+        const allLanguagesAvailable = languagesArray.join(', ');  
         
-        markupCountryInfo(element, allLanguagesAvailable);
-
-        markupCountryList(flags, name);
+        if (data.length === 1) {
+            markupCountryInfo(element, allLanguagesAvailable); 
+            countryList.innerHTML = '';
+            
+        } else {
+            markupCountryList(flags, name); 
+            countryInfo.innerHTML = '';
+        }            
 }); 
 }
-
 
 function markupCountryList(flags, name) {
     const markupCountryList = `<div class="country-wrap">
     <li class="contry-item"><img src="${flags.svg}" alt="" width="40" height="30"></li>
     <li class="contry-item">${name}</li></div>`;
-    
-    countryList.insertAdjacentHTML('beforeend', markupCountryList)
-    }
 
-
-
+    countryList.insertAdjacentHTML('beforeend', markupCountryList);
+}
 
 function markupCountryInfo(element,languagesValue) {
-
     const {flags,name,population,capital} = element;
 
-    const markupCountryInfo = `<ul><div class="country-wrap">
+    const markupCountryInDiv = `<ul><div class="country-wrap">
     <li class="contry-item"><img src="${flags.svg}" alt="" width="40" height="30"></li>
     <li class="contry-item font-size">${name}</li>
     </div>
     <li class="contry-item">Capital: ${capital}</li>
     <li class="contry-item">Population: ${population}</li>
     <li class="contry-item">Languages: ${languagesValue}</li></ul>`;
-    
-    countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo);
+   countryInfo.insertAdjacentHTML('beforeend', markupCountryInDiv);   
 }
-        
