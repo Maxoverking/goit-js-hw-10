7a67d7d3dfba0ed81c7144f79f88a40b3721983c
Maxoverking <avemax7@gmail.com>
2022-11-05 13:13:24 +0100

    добавил описание всего сделаного

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+//импорт fetch , import fetch
 import {fetchCountries} from './fetchCountries'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
@@ -13,6 +14,8 @@ const input = document.getElementById('search-box');
 
 input.addEventListener('input', debounce(inSearchCountries, DEBOUNCE_DELAY));
 
+// создаем функцию по поиску даных из input
+// create a function to search for data from input
 function inSearchCountries(evt) {   
     evt.preventDefault();
 
@@ -21,9 +24,15 @@ function inSearchCountries(evt) {
        cleanDocument();; 
     return;
     }
+    //функция очистки HTML документа
+    // function to clean up an HTML document
     cleanDocument(); 
 
+    //импортированая функция fetch с возвратом json promise
+    //imported fetch function with json promise return
     fetchCountries(inputValue)
+        // получаем даные
+        // get the data
         .then(data => {
 
         if (data.length >= 10) {
@@ -36,28 +45,35 @@ function inSearchCountries(evt) {
         } else {
             listOfAllCountries(data);
             }
-        }).catch(error =>
-            Notify.failure("Oops, there is no country with that name",
+        }).catch(error => //получаем ошибку  we get an error
+           Notify.failure("Oops, there is no country with that name",
                 { position: "center-top", width: '420px', fontSize: '20px' }))
 }
 
+//создаем разметку для всех стран
+// create a mark-up for all countries
 function listOfAllCountries(data) {
-    data.forEach(object => {        
+    const markupAllCountry = data.map(object => {//можно map через переменую 
+                                                //только нужен join в конце
         const { flags, name } = object;
         
-        const markupAllCountry = 
-        `<li class="country-row">
+       return `<li class="country-row">
         <img src="${flags.svg}" alt="${name}" width="40" height="30">
         ${name}</li>`;
-    countryList.insertAdjacentHTML('beforeend', markupAllCountry);
-    })
+    }).join('')
+
+    // console.log("🚀 ~ ma", markupAllCountry);
+    countryList.innerHTML =  markupAllCountry;
 }
 
+//создаем разметку для одной страны
+//create the mark-up for one country
 function oneCountry(data) {
-        data.forEach(object => {
+    data.forEach(object => { // или попробовать через forEach
+                            //or try forEach
             const { flags, name, population, capital } = object;
             
-            let languagesArray = [];           
+            let languagesArray = [];  
             object.languages.forEach(el => {
             const allValueLanguages = [Object.values(el.name).join('')];
             languagesArray.push(...allValueLanguages);             
